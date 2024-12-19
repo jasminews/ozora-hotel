f7db50af1a65ce069af14b76270984f412e10eb1
jasminews <jasmine.w.salsabila@gmail.com>
2024-12-19 15:54:09 +0700

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -10,7 +10,7 @@ function showSlidesTop() {
     slideIndexTop++;
     if (slideIndexTop > slidesTop.length) { slideIndexTop = 1 }
     slidesTop[slideIndexTop - 1].style.display = "block";
-    setTimeout(showSlidesTop, 5000); // Change image every 5 seconds
+    setTimeout(showSlidesTop, 3000); // Change image every 3 seconds
 }
 
 let slideIndex = [1, 1, 1, 1];
@@ -43,4 +43,24 @@ function showSlides(n, no) {
     }
     x[slideIndex[no] - 1].style.display = "block";
     dots[slideIndex[no]-1].className += " active";
+}
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
